6d0e0ac6e77c3b42cbda13e8492a45befe40eb9e
Bernardo de Lima Hack <102930809+Bernardo-Hack@users.noreply.github.com>
2023-06-06 10:21:09 -0300

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,27 @@
+public abstract class Person {
+    private String name;
+    private String cpf;
+    private String phoneNumber;
+
+    public Person(String name, String cpf, String phone) {
+        this.name = name;
+        this.cpf = cpf;
+        this.phoneNumber = phone;
+    }
+
+    public void changeNumber(String num) {
+        this.phoneNumber = num;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public String getTelefone() {
+        return this.phoneNumber;
+    }
+}
