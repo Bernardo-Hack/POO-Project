dd2377a5a6b1cd2d30dbf95f5775fee4bfc73208
Bernardo de Lima Hack <102930809+Bernardo-Hack@users.noreply.github.com>
2023-06-06 10:49:12 -0300

    Create Baker.java

---
+++ Baker.java
@@ -0,0 +1,21 @@
+public class Baker extends Person implements Interface_Storage{
+    public Baker(String name, String cpf, String phone) {
+        super(name, cpf, phone);
+    }
+
+    @Override
+    public void checkIngredientStorage() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void checkProductStorage() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void makeProduct(String name) {
+        
+    }
+}
