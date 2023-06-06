2195b668a7ee3bb74ab4481a9d6191f420d9bbe8
Bernardo de Lima Hack <102930809+Bernardo-Hack@users.noreply.github.com>
2023-06-06 10:49:17 -0300

    Update Ingredient.java

--- Ingredient.java
+++ Ingredient.java
@@ -1,20 +1,20 @@
 public class Ingredient {
     private String name;
-    private double value;
+    private double value; // value se refere ao custo de compra do ingrediente(ex.: R$50/L ou R$50/Kg)
     private int quant;
-    private String medida;
+    private String measuringUnit;
 
-    public Ingredient(String name, double value, int quant, String medida) {
+    public Ingredient(String name, double value, int quant, String measure) {
         this.name = name;
         this.value = value;
         this.quant = quant;
-        this.medida = medida;
+        this.measuringUnit = measure;
     }
 
     public void printIngredient() {
         System.out.println("Nome: " + this.getName() +
                          "\nValor: R$ " + this.getValue() +
-                         "\nQuantidade: " + this.getQuant() + this.medida);
+                         "\nQuantidade: " + this.getQuant() + this.measuringUnit);
     }
 
     public String getName() {
@@ -29,7 +29,7 @@ public class Ingredient {
         return this.quant;
     }
 
-    public String getMed() {
-        return this.medida;
+    public String getMeasuringUnit() {
+        return this.measuringUnit;
     }
 }
