02bd773d25012ee4214d5d039a73569d81c57ecc
Bernardo de Lima Hack <102930809+Bernardo-Hack@users.noreply.github.com>
2023-06-06 10:21:01 -0300

    Create Ingredient.java

---
+++ Ingredient.java
@@ -0,0 +1,35 @@
+public class Ingredient {
+    private String name;
+    private double value;
+    private int quant;
+    private String medida;
+
+    public Ingredient(String name, double value, int quant, String medida) {
+        this.name = name;
+        this.value = value;
+        this.quant = quant;
+        this.medida = medida;
+    }
+
+    public void printIngredient() {
+        System.out.println("Nome: " + this.getName() +
+                         "\nValor: R$ " + this.getValue() +
+                         "\nQuantidade: " + this.getQuant() + this.medida);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    
+    public double getValue() {
+        return this.value;
+    }
+    
+    public int getQuant() {
+        return this.quant;
+    }
+
+    public String getMed() {
+        return this.medida;
+    }
+}
