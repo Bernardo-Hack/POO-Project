4316246bd8ec0a5cff4f10a6f9b5cfaa9bda6490
Bernardo de Lima Hack <102930809+Bernardo-Hack@users.noreply.github.com>
2023-06-06 10:49:14 -0300

    Create Confectioner.java

---
+++ Confectioner.java
@@ -0,0 +1,21 @@
+public class Confectioner extends Person implements Interface_Storage {
+    public Confectioner(String name, String cpf, String phone) {
+        super(name, cpf, phone);
+    }
+
+    @Override
+    public void checkIngredientStorage() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void checkProductStorage() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void makeProduct(String name) {
+        
+    }
+}
