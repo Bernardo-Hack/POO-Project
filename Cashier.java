30d52de9991cd098f5b19a6232ee236741bb7467
Gabriela Ribeiro <gr08.ribeiro@gmail.com>
2023-06-15 21:29:23 -0300

    Update Cashier.java

--- Cashier.java
+++ Cashier.java
@@ -2,8 +2,4 @@ public class Cashier extends Person {
     public Cashier(String name, String cpf, String phone) {
         super(name, cpf, phone);
     }
-
-    public void registerClient() {
-        
-    }
 }
